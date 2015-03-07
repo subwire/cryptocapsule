eccfcabcb43a40cf014e659a177e348f759c0b19
Jason Berry <bloodlustr@gmail.com>
2015-03-07 15:01:35 -0800

    Added setup and initial socket creation

--- cryptocapsule/server.py
+++ cryptocapsule/server.py
@@ -2,7 +2,7 @@ import socket
 import os
 import re
 import optparse
-from cryptoutils import gen_temporal_keypair
+from cryptoutils import gen_temporal_keypair, generate_master_secret, generate_key_cert
 from netutils import *
 from base64 import b64decode, b64encode
 from Crypto.Util.number import bytes_to_long, long_to_bytes
@@ -61,16 +61,27 @@ def listen(port, tlsprivkey, tlspubkey, masterkey):
 
 
 def parse_opts():
-    parser = optparse.OptionParser(usage="Usage: ccapsuled [opts] infile outfile")
+    parser = optparse.OptionParser(usage="Usage: python server.py -k KEYFILE -c CERTFILE -s SECRETFILE [opts]")
 
     options = optparse.OptionGroup(parser ,"OPTIONS")
     options.add_option("-p", help="Port to listen on", type="int", default=31337, dest="port")
     options.add_option("--setup", help="Do first-run setup", action="store_true", dest="setup")
-    options,add_option("-s", "--secret", help="Path to the master secret", dest="secretfile")
+    options.add_option("-s", "--secret", help="Path to the master secret", dest="secretfile")
+    options.add_option("-k", "--key", help="Path to the key", dest="keyfile")
+    options.add_option("-c", "--cert", help="Path to the certificate", dest="certfile")
     
     parser.add_option_group(options)
 
     opts, args = parser.parse_args()
+    
+    if not opts.keyfile:
+        parser.error("key file path required")
+    
+    if not opts.certfile:
+        parser.error("cert file path required")
+
+    if not opts.secretfile:
+        parser.error("secret file path required")
 
     return opts, args
 
@@ -78,7 +89,13 @@ def parse_opts():
 
 if __name__ == '__main__':
     # TODO: Load master secret, load TLS keys, check clocks
-    s=socket.socket()
-    sslSocket=ssl.wrap_socket(s,keyfile='keyfile',certfile='certfile',ssl_version=ssl.PROTOCOL_TLSv1_2)
     opts, args = parse_opts()
-    listen(opts.port, None, None, None)
+    if opts.setup:
+        generate_master_secret(opts.secretfile)
+        generate_key_cert(opts.keyfile,opts.certfile)
+    else:
+        secretfile=open(opts.secretfile)
+        secret=base64.b64decode(secretfile.read())
+        s=socket.socket()
+        sslSocket=ssl.wrap_socket(s,keyfile=opts.keyfile,certfile=opts.certfile,ssl_version=ssl.PROTOCOL_TLSv1_2)
+        listen(opts.port, None, None, None)
