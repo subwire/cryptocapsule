46970fd1c970415035d3aad448751069e5d2a0fc
Eric Gustafson <edg@cs.ucsb.edu>
2015-03-07 15:03:11 -0800

    Implement

--- cryptocapsule/netutils.py
+++ cryptocapsule/netutils.py
@@ -5,9 +5,35 @@ import struct
 import time
 from cryptoutils import gen_temporal_keypair
 import base64
-
+import ssl
+import ssl
 
 # Client network functions
+
+
+def check_host_name(peercert, name):
+    """Simple certificate/host name checker.  Returns True if the
+    certificate matches, False otherwise.  Does not support
+    wildcards."""
+    # Check that the peer has supplied a certificate.
+    # None/{} is not acceptable.
+    if not peercert:
+        return False
+    if peercert.has_key("subjectAltName"):
+        for typ, val in peercert["subjectAltName"]:
+            if typ == "DNS" and val == name:
+                return True
+    else:
+        # Only check the subject DN if there is no subject alternative
+        # name.
+        cn = None
+        for attr, val in peercert["subject"]:
+            # Use most-specific (last) commonName attribute.
+            if attr == "commonName":
+                cn = val
+        if cn is not None:
+            return cn == name
+    return False
 
 
 def select_server(serverlist):
@@ -22,6 +48,15 @@ def select_server(serverlist):
         yield server
 
 
+def recvall(sock):
+    buf = ""
+    newdata = "abcd"
+    while len(newdata) > 0:
+        newdata = sock.recv(2048)
+        buf += newdata
+    return buf
+
+
 def send_query(buf, server):
     """
     Send query in buf to server
@@ -30,6 +65,22 @@ def send_query(buf, server):
     :param buf:
     :return: buf with a reply, or None on error
     """
+    try:
+        s = socket.socket()
+        s.connect((server, 31337))
+        sslsock = ssl.wrap_socket(s)
+        # TODO: more thorough SSL verification
+        if not check_host_name(sslsock.getpeercert(), server):
+            raise IOError("peer certificate does not match host name")
+
+        sslsock.sendall(buf)
+
+        response = recvall(sslsock)
+        return response
+
+    # TODO: Better error handling
+    except:
+        return None
 
 
 def get_pubkey(server, dectime, salt):
@@ -40,13 +91,16 @@ def get_pubkey(server, dectime, salt):
     :param salt: The salt, 128-byte long
     :return: Return pubkey
     """
+    msg = " ".join(["GETPRIV:", str(dectime), base64.b64encode(salt)]) + "\n"
+    return send_query(msg,server)
+    """
     # WARNING: Don't forget the base64 the binary stuff first
     # TODO: IMPLEMENT THIS
     # Should put the args together, and call send_query
     fake_masterkey = "blahblahblah"
     _, pubkey = gen_temporal_keypair(fake_masterkey, dectime, salt)
     return pubkey
-
+    """
 
 def get_privkey(server, dectime, salt):
     """
@@ -56,7 +110,10 @@ def get_privkey(server, dectime, salt):
     :param salt: The salt, 128-byte long
     :return: Return privkey, or None on failure
     """
-    # WARNING: Don't forget to base64 the binary stuff first
+    msg = " ".join(["GETPRIV:", str(dectime), base64.b64encode(salt)]) + "\n"
+    return send_query(msg,server)
+
+    """
     # TODO: IMPLEMENT THIS
     # Should put the args together, and call send_query
     fake_masterkey = "blahblahblah"
@@ -64,9 +121,10 @@ def get_privkey(server, dectime, salt):
         privkey, _ = gen_temporal_keypair(fake_masterkey,dectime,salt)
         return privkey
     return None
-
+    """
 
 # Server functions
+
 
 def send_reply(buf, sock):
     """
@@ -75,7 +133,7 @@ def send_reply(buf, sock):
     :param sock: The socket
     :return:
     """
-    sock.sendAll(buf)
+    sock.sendall(buf)
 
 
 def send_privkey(key, sock):
@@ -86,7 +144,7 @@ def send_privkey(key, sock):
     :return:
     """
     buf = "PRIVKEY: " + base64.b64encode(key) + "\n"
-    send_reply(buf,sock)
+    send_reply(buf, sock)
 
 
 def send_pubkey(key, sock):
@@ -97,7 +155,7 @@ def send_pubkey(key, sock):
     :return:
     """
     buf = "PUBKEY: " + base64.b64encode(key) + "\n"
-    send_reply(buf,sock)
+    send_reply(buf, sock)
 
 
 def send_error(msg, sock):
@@ -108,7 +166,28 @@ def send_error(msg, sock):
     :return:
     """
     buf = "FAIL: " + msg + "\n"
-    send_reply(buf,sock)
+    send_reply(buf, sock)
+
+
+def read_line(sock, maxchars=1000):
+    """
+    Read a full line from a socket.  If a \n does not appear in maxchars,
+    return None.  If the client stops sending before we get a \n,
+    return None
+    :param sock:
+    :param maxchars:
+    :return:
+    """
+    chrs = 0
+    line = ""
+    while chrs < maxchars:
+        c = sock.read(1)
+        if len(c) != 1:
+            return None
+        if c == "\n":
+            return line
+        line += c
+    return None
 
 
 def recv_query(sock):
@@ -124,4 +203,6 @@ def recv_query(sock):
     :return: Tuple (command, arg1, arg2, ...) or None on failure
     """
 
-
+    line = read_line(sock)
+    cmd = line.split(" ")
+    return cmd
